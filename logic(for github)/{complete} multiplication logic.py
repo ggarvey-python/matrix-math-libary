c61a09a1ea4ba2ddf58f6bf999e92e6d3a6e2bdc
ggarvey-python <43453367+ggarvey-python@users.noreply.github.com>
2018-10-13 17:33:13 +0100

    Add files via upload

--- logic(for github)/{complete} multiplication logic.py
+++ logic(for github)/{complete} multiplication logic.py
@@ -4,13 +4,13 @@ import numpy as np
 #NOT THE COMPLETE APP
 
 
-test_matrix = [[5,2,2],
-               [9,3,9],
-               [5,5,4]]
+test_matrix = [[8,4,2,2],
+               [6,2,5,7]]
 
-test_matrix2 = [[6,3,8],
-                [3,5,2],
-                [3,5,8]]
+test_matrix2 = [[4,2],
+                [2,3],
+                [9,1],
+                [6,3]]
 
 
 ## to see the amount of numbers in each matrix
@@ -22,14 +22,7 @@ print('numbers in columns (matrix 2) : {}'.format(columns2))
 
 
 ###########################################################################
-# the amount of rows in eact matrix
-##num_of_row = []
-##
-##for num in test_matrix[0]:
-##    num_of_row.append(num)
-###this one deos the counting
-##num_of_row = len(num_of_row)
-##print('number of rows in matrix 1 : {}'.format(num_of_row))
+
 ##########################################################################
 ##
 num_of_row2 = []
@@ -41,22 +34,7 @@ num_of_row2 = len(num_of_row2)
 print('number of rows in matrix 1 : {}'.format(num_of_row2))
 
 #######################################################################
-##rows is the rows in matrix1 placed out in a line for simplicity
 
-##rows = []
-##
-##for i in range(len(test_matrix[0])):                                  
-##    for x in range(len(test_matrix)):
-##        
-##        rows.append(test_matrix[x][i])
-##        
-##print('rows 1 : {}'.format(rows))
-
-
-
-########################################################################
-## rows2 is the rows in matrix2 placed out in a line for simplicity
-##logic is a bit rough will fix later
 rows2 = []
 rows2_format = []
 for i in range(len(test_matrix2[0])):                                  
@@ -73,7 +51,7 @@ for i in range(len(test_matrix2[0])):
 print('rows 2 : {}'.format(rows2_format))
 
 ############################################################################
-#a list of everything in the all the columns of matrix 1
+##a list of everything in the all the columns of matrix 1
 
 column_list = []
 for i in range(len(test_matrix)):
@@ -89,9 +67,6 @@ for i in range(len(test_matrix2)):
     for x in range(len(test_matrix2[0])):
       
         column_list2.append(test_matrix2[i][x])
-
-    
-
         
 print('columns 1 : {}'.format(column_list))
 print('columns 2 : {}'.format(column_list2))
@@ -112,32 +87,32 @@ elif columns == columns2:
     largest = columns
     smallest = columns2
 else:
-    print('either failed or magic')
+    print('either failed')
 
 list_of_multipyed_values = []
 
 ##the most efficent way to multiply the matrices out
 
 
-for i in range(largest):
-    for ii in range(largest):
+for i in range(smallest):
+    for ii in range(smallest):
         list_of_multipyed_values.append(np.dot(np.array(test_matrix[i]), rows2_format[ii]))
 
 t = np.array(list_of_multipyed_values)
 #splits an arrat 9 is the expexted number of values
 
-expected_values = smallest * smallest
-finished = np.array_split(t, largest*largest)
+
+finished = np.array_split(t, smallest*2)
 
 
 #i in range of expected number of values at the end
 finished_multiplyed = []
-for i in range(largest*largest):
+for i in range(smallest*2):
     finished_multiplyed.append(sum(finished[i]))
 
     
 finished_multiplyed = np.array(finished_multiplyed)
-finished_multiplyed = np.array_split(finished_multiplyed, num_of_row2)
+finished_multiplyed = np.array_split(finished_multiplyed, smallest*2)
 
 finished_multiplyed = np.array(finished_multiplyed)
 
