947dbdfef32bd5962827b18e9d6006a269875bcd
ggarvey-python <43453367+ggarvey-python@users.noreply.github.com>
2018-10-15 21:51:06 +0100

    Update the app(backend).py

--- the app(github)/the app(backend).py
+++ the app(github)/the app(backend).py
@@ -1,17 +1,1 @@
-##welcome to the full app
-'''
-welcome to the full app
-this is a matrix math libary
-you are currently viewing the back end
-i would like someone to build the front end
 
-this apps purpose is to multipy divide add and subtract matrieces
-i will most liklty add more functions later
-'''
-
-matrix1 = input('numers')
-matrix2 = input('numers')
-
-
-def adding(matrix1, matrix2):
-    
